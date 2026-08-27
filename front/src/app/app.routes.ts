951f119b551ea6e7c8f7f4915c827abfee59a06b
Samuel Bart <bartsamuel@me.com>
2026-08-27 09:58:39 +0200

    feat(front): add auth routes and login/register components

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+];
---
+++ front/src/app/features/auth/pages/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-register',
+  styleUrl: './register.component.scss',
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {}
