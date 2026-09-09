053773e1805743374fbfb64c280bf23310ed56d8
Samuel Bart <bartsamuel@me.com>
2026-09-09 11:07:43 +0200

    feat:front(home): add redirect when user logged in

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,16 +1,25 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MaterialComponents } from '@shared/ui/material';
 import { AuthService } from '../../features/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MaterialComponents],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   protected readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
   readonly isLoggedIn = this.authService.isLoggedIn;
+
+  constructor() {
+    effect(() => {
+      if (this.isLoggedIn()) {
+        this.router.navigate(['/posts/feed']);
+      }
+    });
+  }
 }
