c27696ac43d7e4794b62f90c2f8067f8675c4953
Samuel Bart <bartsamuel@me.com>
2026-09-11 18:26:27 +0200

    fix(front) fix sonarqube recommandation on cypress config

--- front/cypress/coverage.esbuild.cjs
+++ front/cypress/coverage.esbuild.cjs
@@ -1,6 +1,6 @@
 const path = require('node:path');
 
-module.exports = builderOptions => {
+module.exports = function configureCoverage(builderOptions) {
   const sourceDirectory = `${path.resolve('src')}${path.sep}`;
 
   builderOptions.instrumentForCoverage = filename => filename.startsWith(sourceDirectory);
